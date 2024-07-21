7a43273798d586357201a59fb474b59aa09c6cd1
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-07-21 11:55:37 -0300

    exercicio de fixação

---
+++ exercicio_uri_1009/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+string nomeVendedor;
+double salarioFixo, vendasEfetuadasMes, salarioTotal;
+
+nomeVendedor =  Console.ReadLine();
+salarioFixo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+vendasEfetuadasMes = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); 
+
+salarioTotal = vendasEfetuadasMes * 15.0 /100 + salarioFixo;// aqui fazemos o calculo de 15% do valor das vendas efetuadas no mes mais o salario do funcionario 
+
+Console.WriteLine("TOTAL = R$ " + salarioTotal.ToString("F2", CultureInfo.InvariantCulture));
+
+Console.ReadLine();
+
