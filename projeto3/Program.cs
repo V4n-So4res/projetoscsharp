9b29bf9bbb8d1855609e5ef298e24ea9143509ea
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-07-21 10:57:01 -0300

    newrepository

---
+++ projeto3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+string nome1, nome2;
+int id1, id2;
+double iMedia;
+
+string[] pessoas;
+
+pessoas = Console.ReadLine().Split(' ');
+nome1  = pessoas[0];
+id1 = int.Parse(pessoas[1]);
+
+pessoas = Console.ReadLine().Split(' ');
+nome2  = pessoas[0];
+id2 = int.Parse(pessoas[1]);
+
+iMedia = (double) (id1 + id2) / 2;
+
+Console.WriteLine("A idade média de " + nome1 + " e " + nome2 + " é de " + iMedia + " anos.");
+
+Console.ReadLine();
+
+
