f9e0046fcb86ecba190a53f99a55f4dae968aa36
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-07-21 11:37:24 -0300

    operador mod(%)

--- projeto3/Program.cs
+++ projeto3/Program.cs
@@ -1,20 +1,20 @@
 ﻿using System.Globalization;
 
-string nome1, nome2;
-int id1, id2;
-double iMedia;
+string nome1, nome2;// declaramos a variavel nome
+int id1, id2;// declaramos as variaveis inteiras
+double iMedia;// variavel flutuante
 
-string[] pessoas;
+string[] pessoas;// criamos um vetor 
 
-pessoas = Console.ReadLine().Split(' ');
+pessoas = Console.ReadLine().Split(' ');// leitura de mais um dado na mesma linha 
 nome1  = pessoas[0];
 id1 = int.Parse(pessoas[1]);
 
-pessoas = Console.ReadLine().Split(' ');
+pessoas = Console.ReadLine().Split(' ');// leitura de mais um dado na mesma linha 
 nome2  = pessoas[0];
 id2 = int.Parse(pessoas[1]);
 
-iMedia = (double) (id1 + id2) / 2;
+iMedia = (double) (id1 + id2) / 2.0;// tivemos que adicionar a informação double pra realizar o casting e transformar em número flutuante 
 
 Console.WriteLine("A idade média de " + nome1 + " e " + nome2 + " é de " + iMedia.ToString("F1", CultureInfo.InvariantCulture) + " anos.");
 
---
+++ projeto4/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Leia um valor inteiro, queé o tempo de duração em segundos de um determinado evento em uma fábrica e informe-o expresso no formato hora minutos e segundos.
+
+Entrada 
+
+O arquivo de entrada contém um valor inteiro N.
+
+Saída 
+
+Imprima o tempo lido no arquivo de entrada (segundos), convertido para horas: minutos: segundos, conforme exemplo fornecido.*/ 
+
+int N, horas, resto, minutos, segundos; // declarei as variaveis 
+
+N = int.Parse(Console.ReadLine());// solicitei que o usuario digite um valor
+
+horas = N / 3600;// divisao do valor digitado pelo usuario pela quantidade de horas que transformadas em segundos é equivalente a 3.600
+resto = N % 3600;// o resto da divisao do valor de N por 3.600 vai ficar armazenado nessa variavel 
+
+minutos = resto / 60;// minutos recebe o resto da divisao das horas divido por 60 segundos para apresentar os minutos 
+segundos = resto % 60;// e segundos recebe o resto da divisão mod (%) 60 que mostrara os segundos 
+
+Console.WriteLine(horas + ":" + minutos + ":" + segundos);
+Console.ReadLine(); 
