7affad60cbc4820599406820d5bdacf235709c80
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-07-24 21:13:38 -0300

    upp

--- projeto2/obj/Debug/net8.0/projeto2.AssemblyInfo.cs
+++ projeto2/obj/Debug/net8.0/projeto2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projeto2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a43273798d586357201a59fb474b59aa09c6cd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ba79d4b81bdb79063ebc62121786b5673bfe02f")]
 [assembly: System.Reflection.AssemblyProductAttribute("projeto2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projeto2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
